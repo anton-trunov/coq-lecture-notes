b0bf78837243e37d9af90c52589b6faf8cc27c44
Anton Trunov <anton.a.trunov@gmail.com>
2019-09-17 19:36:01 +0300

    Add exercises for homework 01

---
+++ homework/hw01.v
@@ -0,0 +1,65 @@
+From mathcomp Require Import ssreflect ssrfun ssrbool ssrnat.
+
+(*** Exercise 1 *)
+
+(** 1a. Define [orb_my] function implementing boolean disjunction *)
+
+(** 1b. Figure out the implementation of [orb] function in the standard library
+        using Coq's interactive query mechanism *)
+
+(** 1c. What's the difference between the standard implementation and
+        the following one? *)
+
+Definition orb_table (b c : bool) : bool :=
+  match b, c with
+  | true,  true  => true
+  | true,  false => true
+  | false, true  => true
+  | false, false => false
+  end.
+
+(** Note: the above translates into nested pattern-matching, check this *)
+
+
+(** 1d. Define [addb_my] function implementing exclusive boolean disjunction.
+        {The name [addb] comes from the fact this operation behaves like addition modulo 2}
+        If you are already familiar with proofs in Coq, think of a prover friendly
+        definition, if you are not -- experiment with how different implementations
+        behave under symbolic execution. *)
+
+
+(*** Exercise 2 *)
+
+(** 2a. Implement power function of two arguments [x] and [n],
+        raising [x] to the power of [n] *)
+
+
+(*** Exercise 3 *)
+
+(** 3a. Implement division on unary natural numbers *)
+
+(** 3b. Provide several unit tests using [Compute] vernacular command *)
+
+
+(*** Exercise 4 *)
+
+(** Use the [applyn] function defined during the lecture
+    (or better its Mathcomp sibling called [iter]) define
+
+    4a. addition on natural numbers
+
+    4b. multiplication on natural numbers
+
+    Important: don't use recursion, i.e. no [Fixpoint] / [fix] should be in your solution.
+*)
+
+(** Here is out definition of [applyn]: *)
+Definition applyn (f : nat -> nat) :=
+  fix rec (n : nat) (x : nat) :=
+    if n is n'.+1 then rec n' (f x)
+    else x.
+
+
+
+
+
