9158279c4e0198842ebf9045113ec9eabeba04b7
Anton Trunov <anton.a.trunov@gmail.com>
2019-10-11 14:56:31 +0300

    Add lecture05 and seminar05

---
+++ code/lecture05.v
@@ -0,0 +1,362 @@
+From mathcomp Require Import ssreflect ssrfun ssrbool eqtype ssrnat seq.
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+
+(*** [reflect] predicate *)
+
+Section MotivationalExample.
+
+Variable T : Type.
+
+Variable p : pred T.
+Print pred.
+Check p : T -> bool.
+
+Lemma all_filter (s : seq T) :
+  all p s -> filter p s = s.
+Proof.
+
+(* Notation "[ 'seq' x <- s | C ]" := (filter (fun x => C) s) *)
+
+Print all.
+Print filter.
+
+by elim: s => //= x s IHs /andP[-> /IHs->].
+Qed.
+
+End MotivationalExample.
+
+
+(** How does [andP] from above work? *)
+
+About andP.
+
+Print reflect.
+Print Bool.reflect.
+
+(**
+    Inductive reflect (P : Prop) : bool -> Set :=
+    | ReflectT : P -> reflect P true
+    | ReflectF : ~ P -> reflect P false
+ *)
+
+Search _ reflect.
+
+(** First, let us show that [P] if and only if [b = true]
+    as two separate lemmas
+  *)
+
+Lemma introT_my (P : Prop) (b : bool) :
+  reflect P b -> P -> b.
+Proof.
+Set Printing Coercions.
+rewrite /is_true.
+Unset Printing Coercions.
+case.
+- move=> _ _. done.
+move=> np. move/np. case.
+Qed.
+
+Lemma elimT_my (P : Prop) (b : bool) :
+  reflect P b -> b -> P.
+Proof.
+case.
+- move=> p _. exact: p.
+move=> _. done.
+Qed.
+
+(** Essentially, a [reflect] predicate connects
+    a _decidable_ proposition to its decision procedure.
+ *)
+Lemma reflect_lem P b :
+  reflect P b -> P \/ ~ P.
+Proof.
+by case=> H; [left | right].
+Qed.
+
+(** Lets look at some standard [reflect] predicates *)
+
+Lemma andP_my (b c : bool) :
+  reflect (b /\ c) (b && c).
+Proof.
+by case: b; case: c; constructor=> //; case.
+Qed.
+
+
+Lemma orP_my (b c : bool) :
+  reflect (b \/ c) (b || c).
+Proof. (* exercise *) Admitted.
+
+Lemma nandP_my b c : reflect (~~ b \/ ~~ c) (~~ (b && c)).
+Proof. by case: b; case: c; constructor; auto; case. Qed.
+
+
+
+(** * Using reflection views in intro patterns *)
+
+Lemma special_support_for_reflect_predicates b c :
+  b && c -> b /\ c.
+Proof.
+move/andP.
+Show Proof.
+
+(**
+SSReflect implicitly inserts the so-called view hints to
+facilitate boolean reflection. In this case it's [elimTF] view hint.
+
+Here is the syntax to do that (see ssrbool.v file):
+Hint View for move/ elimTF|3 elimNTF|3 elimTFn|3 introT|2 introTn|2 introN|2.
+
+The optional natural number is the number of implicit arguments
+to be considered for the declared hint view lemma.
+
+The ssrbool.v module already declares a numbers of view hints,
+so adding new ones should be justtified. For instance, one might need to do it
+if one defines a new logical connective.
+ *)
+
+(* Set Printing All. *)
+(* Show Proof. *)
+
+About elimTF.
+
+Restart.
+
+move=> Hb.
+Check @elimTF (b /\ c) (b && c) true (@andP b c) Hb.
+move: Hb.
+move/(@elimTF (b /\ c) (b && c) true (@andP b c)).
+
+exact: id.
+Qed.
+
+
+(** Reflection views generally work in both directions *)
+Lemma special_support_for_reflect_predicates' (b c : bool) :
+  b /\ c -> b && c.
+Proof.
+move/andP.
+Show Proof.
+About introT.  (** [introT] view hint gets implicitly inserted *)
+exact: id.
+Qed.
+
+
+
+(** * Switching views at the goal *)
+
+Lemma special_support_for_reflect_predicates'' (b c : bool) :
+  b /\ c -> b && c.
+Proof.
+move=> ab.
+apply/andP.  (** [apply/] syntax *)
+Show Proof.  (** [introTF] view hint gets inserted *)
+done.
+Qed.
+
+
+
+(** Specification for [eqn] -- decision procedure for equality on [nat]s *)
+Lemma eqnP_my (n m : nat) : reflect (n = m) (eqn n m).
+Proof.
+elim: n m=> [|n IHn] [|m]; try constructor=> //.
+
+move=> /=.
+
+(** Need to convert a [reflect]-based propositions into biimplications *)
+Search reflect -seq -"or" -"and" -"mem" -negb -minn.
+
+Check iffP (IHn m).
+(**
+How the conclusion of [iffP (IHn m)] matches with the goal:
+
+                                         reflect (n.+1 = m.+1) (eqn n m)
+       (n = m -> ?Q) -> (?Q -> n = m) -> reflect ?Q            (eqn n m)
+
+Coq infers [?Q] existential variable to be [n.+1 = m.+1]
+*)
+by apply: (iffP (IHn _)) => [-> | /succn_inj].
+
+Restart.
+
+apply: (iffP idP).   (** [idP] -- the trivial reflection view *)
+- by elim: n m => [|n IHn] [|m] //= /IHn->.
+- move=> ->. by elim: m.
+Qed.
+
+
+Lemma silly_example_iffP_andP (b c : bool) :
+  reflect (b /\ c) (b && c).
+Proof.
+apply: (iffP idP).
+Undo.
+Check (iffP andP).
+apply: (iffP andP).
+done.
+done.
+Qed.
+
+
+(** A better example of using [iffP] with a non-[idP] argument *)
+Lemma nseqP (T : eqType) n (x y : T) :
+  reflect (y = x /\ n > 0) (y \in nseq n x).
+Proof.
+rewrite mem_nseq andbC.
+apply: (iffP andP).
+case=> /eqP->. done.
+case=>->. done.
+
+Restart.
+
+by rewrite mem_nseq andbC; apply: (iffP andP) => -[/eqP].
+Qed.
+
+
+
+(** * Rewriting with [reflect] predicates *)
+
+
+About maxn_idPl.
+
+Lemma leq_max m n1 n2 :
+  (m <= maxn n1 n2) = (m <= n1) || (m <= n2).
+Proof.
+case/orP: (leq_total n2 n1) => [le_n21 | le_n12].
+
+rewrite (maxn_idPl le_n21).
+
+(** Why does this work?
+    [maxn_idPl] is _not_ a function but behaves like one here *)
+
+Check (maxn_idPl le_n21).  (** OK, this is an ordinary equation,
+                               no wonder [rewrite] works. *)
+
+Set Printing Coercions.
+Check (maxn_idPl le_n21).   (** [elimT] get implicitly inserted *)
+Unset Printing Coercions.
+
+About elimT.
+
+(** [elimT] is a coercion from [reflect] to [Funclass],
+    This means it gets inserted when one uses a reflect view as a function.
+  *)
+
+(** Essentially we invoke the following tactic: *)
+
+Undo.
+rewrite (elimT maxn_idPl le_n21).
+Abort.
+
+
+(** * An example of a specification for a [seq] function *)
+
+(** [all] specification *)
+About allP.
+(**
+    forall (T : eqType) (a : pred T) (s : seq T),
+    reflect {in s, forall x : T, a x} (all a s)
+*)
+
+(** Check out some other specs in the [seq] module! *)
+Search _ reflect in seq.
+
+
+
+
+(*** Specs as rewrite rules *)
+
+Example for_ltngtP m n :
+  (m <= n) && (n <= m) -> (m == n) || (m > n) || (m + n == 0).
+Proof.
+by case: ltngtP.
+
+Restart.
+
+case: ltngtP.
+Abort.
+
+
+Module Trichotomy.
+
+Variant compare_nat m n :
+   bool -> bool -> bool -> bool -> bool -> bool -> Set :=
+  | CompareNatLt of m < n : compare_nat m n true false true false false false
+  | CompareNatGt of m > n : compare_nat m n false true false true false false
+  | CompareNatEq of m = n : compare_nat m n true true false false true true.
+
+Lemma ltngtP m n : compare_nat m n (m <= n) (n <= m) (m < n)
+                                   (n < m)  (n == m) (m == n).
+Proof.
+rewrite !ltn_neqAle [_ == m]eq_sym; case: ltnP => [mn|].
+  by rewrite ltnW // gtn_eqF //; constructor.
+rewrite leq_eqVlt; case: ltnP; rewrite ?(orbT, orbF) => //= lt_nm eq_mn.
+  by rewrite ltn_eqF //; constructor.
+by rewrite eq_mn; constructor; apply/eqP.
+Qed.
+
+(** One more example *)
+Lemma maxnC : commutative maxn.
+Proof. by move=> m n; rewrite /maxn; case: ltngtP. Qed.
+
+End Trichotomy.
+
+
+
+
+
+
+(*** Coercions summary *)
+
+
+(** * [is_true] coercion *)
+
+(** We have already been using [is_true] coercion regularly.
+    It's defined in ssrbool.v as follows:
+
+    Coercion is_true : bool >-> Sortclass.
+ *)
+
+(** E.g. [is_true] makes the following typecheck *)
+Check (erefl true) : true.
+
+
+
+(** * [elimT] coercion *)
+
+(**  Allow the direct application of a reflection lemma to a boolean assertion.
+
+    Coercion elimT : reflect >-> Funclass.
+ *)
+
+Section ElimT_Example.
+
+Variables b c : bool.
+Hypothesis H : b || c.
+
+Check orP H.
+Set Printing Coercions.
+Check orP H.
+Unset Printing Coercions.
+
+End ElimT_Example.
+
+
+
+(** * [nat_of_bool] coercion *)
+
+About nat_of_bool.
+
+About leq_b1.
+About mulnb.
+
+About count_nseq.
+
+(** You can learn more using the following search query: *)
+Search _ nat_of_bool.
+
+
+
+
+
---
+++ seminars/seminar05.v
@@ -0,0 +1,113 @@
+From mathcomp Require Import ssreflect ssrfun ssrbool ssrnat div seq.
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+
+
+Section LeftPad.
+
+(**
+What is "leftpad"?
+
+Leftpad is a function that takes a character, a length, and a string, and pads the string to that length.
+It pads it by adding the character to the left.
+
+Compute leftpad 0 5 [:: 1; 2; 3]. (* = [:: 0; 0; 1; 2; 3] *)
+Compute leftpad 0 2 [:: 1; 2; 3]. (* = [:: 1; 2; 3]  *)
+*)
+
+
+(* [def] is the default value, i.e. type T is not empty *)
+Variables (T : Type) (def : T).
+
+(** Define the leftpad function, modeling strings as sequences of elements of type [T] *)
+
+Definition leftpad (c : T) (n : nat) (s : seq T) : seq T. Admitted.
+
+(** The following properties of the leftpad function *)
+
+Lemma length_max_spec c n s :
+  size (leftpad c n s) = maxn n (size s).
+Proof.
+Admitted.
+
+(** Local alias for the [nth] function returning the n-th element of the input sequence *)
+Local Notation nth := (nth def).
+
+Lemma prefix_spec c n s :
+  forall i, i < n - size s -> nth (leftpad c n s) i = c.
+Proof.
+Admitted.
+
+Lemma suffix_spec c n s :
+  forall i, i < size s -> nth (leftpad c n s) (n - size s + i) = nth s i.
+Proof.
+Admitted.
+
+End LeftPad.
+
+
+
+Section MoreInductionExercises.
+
+(** Implement a recursive function performing integer division by 2 *)
+Fixpoint div2 (n : nat) : nat. Admitted.
+(* You might want to uncomment the following: *)
+(* Arguments div2 : simpl nomatch. *)
+
+Lemma nat_ind2' (P : nat -> Prop) :
+  P 0 ->
+  P 1 ->
+  (forall n, P n -> P n.+2) ->
+  forall n, P n.
+Proof.
+Admitted.
+
+Lemma div2_le n : div2 n <= n.
+Proof.
+Admitted.
+
+Lemma div2_correct n :
+  div2 n = n./2.
+Proof.
+Admitted.
+
+
+
+(** Strong induction principle *)
+Lemma lt_wf_ind (P : nat -> Prop) :
+  (forall m, (forall k : nat, (k < m) -> P k) -> P m) ->
+  forall n, P n.
+Proof.
+Admitted.
+
+
+Fixpoint divn_my (n m : nat) {struct n} : nat :=
+  if m is m'.+1 then
+    if n - m' is d.+1 then
+      (divn_my d m).+1
+    else 0
+  else 0.
+
+Lemma divn_my_correct n m :
+  divn_my n m = divn n m.
+Proof.
+Admitted.
+
+
+
+
+Fixpoint fib (n : nat) : nat :=
+  if n is (n''.+1 as n').+1 then fib n'' + fib n'
+  else n.
+Arguments fib n : simpl nomatch.
+
+Lemma fib3 n :
+  fib (3*n).+1 = (fib n.+1)^3 + 3 * fib n.+1 * (fib n)^2 - (fib n)^3.
+Proof.
+Admitted.
+
+End MoreInductionExercises.
+
+
