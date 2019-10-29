eab74f6272673505c9ac8a07a9d41f26628af377
Anton Trunov <anton.a.trunov@gmail.com>
2019-10-29 14:38:45 +0300

    hw03: solutions

--- homework/hw03.v
+++ homework/hw03.v
@@ -13,11 +13,32 @@ Variable DNE : forall A : Prop, ~ ~ A -> A.
 Lemma drinker_paradox (P : nat -> Prop) :
   exists x, P x -> forall y, P y.
 Proof.
-Admitted.
+apply: DNE => not_DP; apply/not_DP.
+exists 0=> _ y.
+apply: DNE => nPy; apply/nPy.
+case: not_DP.
+by exists y => /nPy.
+Qed.
+
+Lemma inhabited_exists A :
+  (exists x : A, True) <-> inhabited A.
+Proof. by split; case. Qed.
+
+(** Bonus: here is a bit more general formulation of the above lemma,
+    we don't have to have predicates over [nat]s, having an inhabited type is enough *)
+Lemma drinker_paradox' (A : Type) (P : A -> Prop) :
+  inhabited A ->
+  exists x, P x -> forall y, P y.
+Proof.
+apply: DNE => not_DP; apply/not_DP.
+case=> a; exists a=> _ y.
+apply: DNE => nPy; apply/nPy.
+case: not_DP.
+by exists y => /nPy.
+Qed.
 
 (* This closes the section, discharging over DNE *)
 End Classical_reasoning.
-
 Check drinker_paradox.
 
 
@@ -37,15 +58,88 @@ End Misc.
 
 Section Arithmetics.
 
-Lemma min_plus_r  n m p  :
+(* an auxiliary lemma to prove min_plus_r *)
+Lemma subn_leq0 n m :
+  minn n m = n -> n - m = 0.
+Proof.
+move=> min.
+(* we need injectivity of addition here *)
+Search _ addn "I".
+apply: (@addnI m).
+rewrite -maxnE.
+rewrite addn0.
+rewrite -min.
+Search _ maxn minn.
+rewrite minKn.
+by [].
+Qed.
+
+Lemma min_plus_r n m p  :
   minn n m = n -> minn n (m + p) = n.
 Proof.
-Admitted.
+move/subn_leq0.
+rewrite minnE.
+Search _ (?n - (?m + ?p)).
+rewrite subnDA.
+move=> ->.
+simpl.
+Search _ subn 0.
+rewrite sub0n.
+rewrite subn0.
+by [].
+
+Restart.
+
+(* a solution using boolean reflection which we will cover later in the course *)
+move=> /minn_idPl/eqP eq0.
+by rewrite minnE subnDA eq0 sub0n subn0.
+
+(* or, alternatively *)
+Restart.
+
+move/minn_idPl => le_nm; apply/minn_idPl.
+by rewrite -(addn0 n) leq_add.
+Qed.
 
 Lemma min_plus_minus m n p :
   minn n m + minn (n - m) p = minn n (m + p).
 Proof.
-Admitted.
+Search _ ((?m <= ?n) || (?n <= ?m)).
+move: (leq_total m n).
+Search _ (_ || _) (_ \/ _).
+move/Bool.orb_prop.  (* vanilla Coq, the idiomatic approach would be move/orP *)
+case.
+- move=> le_mn.
+  rewrite minnE.
+  rewrite -{1}(subnKC le_mn).
+  rewrite -{2}(add0n (n-m)).
+  rewrite subnDr.
+  rewrite subn0.
+  rewrite addn_minr.
+  rewrite subnKC.
+  + done.
+  done.
+- move/minn_idPl.
+  move=> min.
+  move: (min).
+  move/subn_leq0.
+  move=>->.
+  rewrite min0n.
+  rewrite addn0.
+  rewrite min.
+  move: min.
+  by move/min_plus_r->.
+
+Restart.
+
+case: (leqP m n) => [le_mn | /ltnW/minn_idPl min_n].
+- rewrite minnE.
+  rewrite -{1}(subnKC le_mn) -{2}(add0n (n-m)) subnDr subn0.
+  by rewrite addn_minr subnKC.
+- move: (min_n)=> /subn_leq0 ->.
+  rewrite min0n addn0 min_n.
+  by rewrite min_plus_r.
+Qed.
 
 Fixpoint zero (n : nat) : nat :=
   if n is n'.+1 then zero n'
@@ -53,8 +147,7 @@ Fixpoint zero (n : nat) : nat :=
 
 Lemma zero_returns_zero n :
   zero n = 0.
-Proof.
-Admitted.
+Proof. by elim: n. Qed.
 
 (**
 Claim: every amount of postage that is at least 12 cents can be made
@@ -125,10 +218,18 @@ Definition epic (f : A -> B) :=
 
 Lemma surj_epic f : surjective f -> epic f.
 Proof.
-Admitted.
+by case=> g inv_g C g1 g2 => /(eq_compr g); rewrite 2!compA => /(eq_idr inv_g).
+Qed.
 
 Lemma epic_surj f : epic f -> surjective f.
   (** Why is this not provable? *)
+
+(** A short answer: to prove a function surjective we need to explicitly provide
+    its inverse, which is not possible in general. We know nothing about type [A],
+    so we cannot construct a function of type [B -> A]
+    unless there is a contradictory statement in our premises,
+    which is not he case here.
+ *)
 Abort.
 
 End SurjectiveEpic.
@@ -139,18 +240,19 @@ Context {A B C : Type}.
 
 Lemma epic_comp (f : B -> C) (g : A -> B) :
   epic f -> epic g -> epic (f \o g).
-Proof.
-Admitted.
+Proof. by move=> Ef Eg D h1 h2; rewrite -2!compA => /Eg/Ef. Qed.
 
 Lemma comp_epicl (f : B -> C) (g : A -> B) :
   epic (f \o g) -> epic f.
-Proof.
-Admitted.
+Proof. by move=> Efg D h1 h2 /(eq_compr g)/Efg. Qed.
 
 Lemma retraction_epic (f : B -> A) (g : A -> B) :
   (f \o g =1 id) -> epic f.
 Proof.
-Admitted.
+move=> Rfg.
+have: surjective f by exists g.
+by apply: surj_epic.
+Qed.
 
 End EpicProperties.
 
@@ -169,11 +271,20 @@ Definition monic (f : B -> C) :=
 
 Lemma inj_monic f : injective f -> monic f.
 Proof.
-Admitted.
+move=> f_inj A g1 g2 fg12 a.
+by move: (fg12 a)=> /f_inj.
+Qed.
+
+Definition const {A B} (a : A) :=
+  fun _ : B => a.
 
 Lemma monic_inj f : monic f -> injective f.
 Proof.
-Admitted.
+move=> f_mon b1 b2 fb12.
+move: (f_mon B (const b1) (const b2)).
+have H : f \o @const _ B b1 =1 f \o const b2 by move=> b /=; rewrite fb12.
+by move/(_ H b1).
+Qed.
 
 End InjectiveMonic.
 
@@ -184,17 +295,21 @@ Context {A B C : Type}.
 Lemma monic_comp (f : B -> C) (g : A -> B) :
   monic f -> monic g -> monic (f \o g).
 Proof.
-Admitted.
+by move=> Mf Mg D h1 h2; rewrite 2!compA => /Mf/Mg.
+Qed.
 
 Lemma comp_monicr (f : B -> C) (g : A -> B) :
   monic (f \o g) -> monic g.
-Proof.
-Admitted.
+Proof. by move=> Mfg D h1 h2 /(eq_compl f)/Mfg. Qed.
 
 Lemma section_monic (f : B -> A) (g : A -> B) :
   (g \o f =1 id) -> monic f.
 Proof.
-Admitted.
+Proof.
+move=> Sfg.
+have : cancel f g by exact: Sfg.
+by move/can_inj/inj_monic.
+Qed.
 
 End MonicProperties.
 
